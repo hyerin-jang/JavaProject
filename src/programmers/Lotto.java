fd6c21f14e43ffb92d19546162a1c9328972ada3
hyerin-jang <hrjang92@gmail.com>
2022-06-03 22:42:06 +0900

    로또의 최고 순위와 최저 순위

---
+++ src/programmers/Lotto.java
@@ -0,0 +1,21 @@
+package programmers;
+
+import java.util.*;
+
+public class Lotto {
+
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Lotto lotto = new Lotto();
+        int[] lottos = {44, 1, 0, 0, 31, 25};
+        int[] win_nums = {31, 10, 45, 1, 6, 19};
+
+        lotto.solution(lottos, win_nums);
+    }
+
+}
