caefcfce9e14b6926c0e76ac3dd0e4f6609ce260
Rin <hrjang92@gmail.com>
2022-10-05 22:36:59 +0900

    coding-test

--- coding-test/src/programmers/Plus.java
+++ coding-test/src/programmers/Plus.java
@@ -1,2 +1,14 @@
-package programmers;public class Plus {
+package programmers;
+
+import java.util.Scanner;
+
+public class Plus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+
+        System.out.println(a + " + " + b + " = " + c);
+    }
 }
