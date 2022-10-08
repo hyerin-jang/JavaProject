60e042df611d3fef254cece75d6abe1311de71bc
Rin <hrjang92@gmail.com>
2022-10-08 22:36:59 +0900

    coding-test

--- coding-test/src/programmers/EvenOrOdd.java
+++ coding-test/src/programmers/EvenOrOdd.java
@@ -1,2 +1,15 @@
-package programmers;public class EvenOrOdd {
+package programmers;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n%2 == 0) {
+            System.out.println(n + " is even");
+        } else {
+            System.out.println(n + " is odd");
+        }
+    }
 }
