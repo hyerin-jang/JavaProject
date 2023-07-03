28648b96f7cb88e002574b9bb9be1c98da14dc7f
Rin <hrjang92@gmail.com>
2023-07-03 22:50:24 +0900

    codingtest

--- coding-test/src/programmers/PrintString.java
+++ coding-test/src/programmers/PrintString.java
@@ -1,2 +1,11 @@
-package programmers;public class PrintString {
+package programmers;
+
+import java.util.Scanner;
+
+public class PrintString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
 }
