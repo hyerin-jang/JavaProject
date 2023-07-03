824cdc3ef47f8a01bb6b274044a73da81ed2cc0a
Rin <hrjang92@gmail.com>
2023-07-03 23:12:38 +0900

    coding-test

--- coding-test/src/programmers/LowerUpperCase.java
+++ coding-test/src/programmers/LowerUpperCase.java
@@ -1,2 +1,21 @@
-package programmers;public class LowerUpperCase {
+package programmers;
+
+import java.util.Scanner;
+
+public class LowerUpperCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        StringBuilder res = new StringBuilder();
+        for(char c : a.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                res.append(Character.toUpperCase(c));
+            } else {
+                res.append(Character.toLowerCase(c));
+            }
+        }
+
+        System.out.println(res);
+    }
 }
