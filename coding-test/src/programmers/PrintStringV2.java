58c9556d1aaa1b384186e13e8b0a6f407367aa42
Rin <hrjang92@gmail.com>
2023-07-03 22:54:05 +0900

    coding-test

--- coding-test/src/programmers/PrintStringV2.java
+++ coding-test/src/programmers/PrintStringV2.java
@@ -1,2 +1,14 @@
-package programmers;public class PrintStringV2 {
+package programmers;
+
+import java.util.Scanner;
+
+public class PrintStringV2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
 }
