d1c36c266e4032f93ef6423697ccd7f4d3c47b0d
Rin <hrjang92@gmail.com>
2023-07-04 22:11:05 +0900

    coding-test

--- coding-test/src/programmers/PlusString.java
+++ coding-test/src/programmers/PlusString.java
@@ -1,2 +1,13 @@
-package programmers;public class PlusString {
+package programmers;
+
+import java.util.Scanner;
+
+public class PlusString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println(a+b);
+    }
 }
