de6e1c220edb73ced3b3571381890d4e3eee13f9
Rin <hrjang92@gmail.com>
2023-07-04 22:14:13 +0900

    coding-test

--- coding-test/src/programmers/StringRotation.java
+++ coding-test/src/programmers/StringRotation.java
@@ -1,2 +1,13 @@
-package programmers;public class StringRotation {
+package programmers;
+
+import java.util.Scanner;
+
+public class StringRotation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(Character c : a.toCharArray()) {
+            System.out.println(c);
+        }
+    }
 }
