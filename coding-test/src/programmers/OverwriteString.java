d536ddfa862210db1d76c7e0aa6b86bba462b982
Rin <hrjang92@gmail.com>
2023-07-04 22:30:15 +0900

    coding-test

--- coding-test/src/programmers/OverwriteString.java
+++ coding-test/src/programmers/OverwriteString.java
@@ -1,2 +1,8 @@
-package programmers;public class OverwriteString {
+package programmers;
+
+public class OverwriteString {
+    public String solution(String my_string, String overwrite_string, int s) {
+        int i = overwrite_string.length();
+        return my_string.substring(0, s) + overwrite_string + my_string.substring(s + i);
+    }
 }
