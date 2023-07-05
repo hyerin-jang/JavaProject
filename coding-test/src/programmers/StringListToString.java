b69553bdcab968976ed6239751e2494cadf23bff
Rin <hrjang92@gmail.com>
2023-07-05 22:12:40 +0900

    coding-test

--- coding-test/src/programmers/StringListToString.java
+++ coding-test/src/programmers/StringListToString.java
@@ -1,2 +1,10 @@
-package programmers;public class StringListToString {
+package programmers;
+
+public class StringListToString {
+    public static void main(String[] args) {
+        String[] str = {"a","b","c"};
+        String answer = "";
+        answer = String.join("", str);
+        System.out.println(answer);
+    }
 }
