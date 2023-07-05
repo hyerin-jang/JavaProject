f7041acd4d6ec4722fc29294fa5841f92c676ff3
Rin <hrjang92@gmail.com>
2023-07-05 22:29:25 +0900

    coding-test

--- coding-test/src/programmers/RepeatString.java
+++ coding-test/src/programmers/RepeatString.java
@@ -1,2 +1,7 @@
-package programmers;public class RepeatString {
+package programmers;
+
+public class RepeatString {
+    public String solution(String my_string, int k) {
+        return my_string.repeat(k);
+    }
 }
