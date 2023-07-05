8670ee3803acdc40400aeaaa89c5843187b3952a
Rin <hrjang92@gmail.com>
2023-07-05 22:05:49 +0900

    coding-test

--- coding-test/src/programmers/StringMix.java
+++ coding-test/src/programmers/StringMix.java
@@ -1,2 +1,16 @@
-package programmers;public class StringMix {
+package programmers;
+
+public class StringMix {
+    public static void main(String[] args) {
+        String str1 = "aaaaaa";
+        String str2 = "bbbbbb";
+        String answer = "";
+
+        for(int i = 0; i < str1.length(); i++) {
+            char char1 = str1.toCharArray()[i];
+            char char2 = str2.toCharArray()[i];
+            answer += Character.toString(char1) + Character.toString(char2);
+        }
+        System.out.println(answer);
+    }
 }
