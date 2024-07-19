8ea258f5c070c83aeea02fbd22bbed9962783e6f
Rin <hrjang92@gmail.com>
2024-07-19 14:36:58 +0900

    ex08

--- coding-test/src/book/Ex08.java
+++ coding-test/src/book/Ex08.java
@@ -1,4 +1,21 @@
 package book;
 
+import java.util.Stack;
+
 public class Ex08 {
+    public static void main(String[] args) {
+        String ex = "()()";
+        Stack<Character> stack = new Stack<>();
+        char[] charArray = ex.toCharArray();
+
+        for(char c : charArray) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (stack.isEmpty() || stack.pop() == c) {
+                System.out.println("false");
+            }
+        }
+
+        System.out.println(stack.isEmpty());
+    }
 }
